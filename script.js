67537e2ae339a0a324bf14641be0915839d8ca0c
Hernán <hernifleitas235@gmail.com>
2022-09-05 23:13:39 -0300

    Start proyect.

---
+++ script.js
@@ -0,0 +1,10 @@
+let submit = document.getElementById("submit");
+let one = document.getElementById("one");
+let two = document.getElementById("two");
+let three = document.getElementById("three");
+let four = document.getElementById("four");
+let five = document.getElementById("five");
+let youSelected = document.getElementById("youSelected");
+
+
+youSelected.innerHTML = "<div><p>You selected ${number}</p></div>"
